2eec139ccec2c5acf9c6e4bfd865408ae3fddf31
Jacek <stvoreku@gmail.com>
2020-01-26 12:49:45 +0100

    Ilosc 0 i 1 w naglowku

--- generator.py
+++ generator.py
@@ -1,18 +1,18 @@
 import numpy as np
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 LAMBDA = 4
 PAC_NUM_1 = 100
 PAC_NUM_2 = 200
 
 k1 = np.random.poisson(LAMBDA, PAC_NUM_1)
-k2 = np.random.poisson(LAMBDA, PAC_NUM_1)
-# plt.hist(k1)
-# plt.show()
+k2 = np.random.poisson(LAMBDA, PAC_NUM_2)
+plt.hist(k1)
+plt.show()
 
 with open("input_gen.txt", "w") as file:
-    file.write("{} {}\n".format(PAC_NUM_1, PAC_NUM_2))
+    file.write("{} {}\n".format(PAC_NUM_1 + k1.sum(), PAC_NUM_2 + k2.sum()))
     for num in k1:
-        for a in range(0,num):
+        for a in range(0, num):
             file.write('0 ')
         file.write("1 ")
     file.write("\n")
